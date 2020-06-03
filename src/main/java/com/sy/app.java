d9c5a89bd017a7f3b81d31773b9d68173cacdff5
wyym123 <3094939316@qq.com>
2020-06-03 17:01:26 +0800

    人事管理项目

--- src/main/java/com/sy/app.java
+++ src/main/java/com/sy/app.java
@@ -3,7 +3,6 @@ package com.sy;
 public class app {
 
     public static void main(String[] args) {
-        System.out.println("ok");
-        System.out.println("123");
+        System.out.println("new");
     }
 }
